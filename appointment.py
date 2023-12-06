b6c60b4e820f74963f54c786c05a04d4ad80369e
Robert D <152869096+ripple-caster@users.noreply.github.com>
2023-12-06 11:16:38 +0600

    Create appointment.py

---
+++ appointment.py
@@ -0,0 +1,12 @@
+# appointment.py
+from datetime import datetime
+
+class Appointment:
+    def __init__(self, doctor, patient, date_time):
+        self.doctor = doctor
+        self.patient = patient
+        self.date_time = date_time
+
+    def schedule_appointment(self):
+        # Implement appointment scheduling logic
+        pass
